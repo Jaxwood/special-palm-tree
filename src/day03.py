987e747bec0979e9f45aa140811f7474840ffef3
Jacob Lorenzen <jacob@lorenzen.me>
2021-05-15 09:28:56 +0200

    Refactor code

--- src/day03.py
+++ src/day03.py
@@ -1,39 +1,38 @@
+from typing import Tuple
+
+
+def move(s: str, coord: Tuple[int, int]) -> Tuple[int, int]:
+    """calculate next move"""
+    x, y = coord
+    if s == '>':
+        x += 1
+    if s == '<':
+        x -= 1
+    if s == '^':
+        y += 1
+    if s == 'v':
+        y -= 1
+    return (x, y)
+
+
 def houses(path: str) -> int:
     """find all houses visited atleast once"""
     current = (0, 0)
     visited = {current}
     for s in path:
-        x, y = current
-        if s == '>':
-            x += 1
-        if s == '<':
-            x -= 1
-        if s == '^':
-            y += 1
-        if s == 'v':
-            y -= 1
-        current = (x, y)
+        current = move(s, current)
         visited.add(current)
     return len(visited)
 
 
 def robot_houses(path: str) -> int:
     """find all houses visited atleast once
-    with the robot as help"""
+    with the help of the robot"""
     santa = (0, 0)
     robot = (0, 0)
     visited = {santa}
     for i in range(0, len(path)):
-        x, y = santa if i % 2 == 0 else robot
-        s = path[i]
-        if s == '>':
-            x += 1
-        if s == '<':
-            x -= 1
-        if s == '^':
-            y += 1
-        if s == 'v':
-            y -= 1
+        x, y = move(path[i], santa if i % 2 == 0 else robot)
         if i % 2 == 0:
             santa = (x, y)
         else:
