cec71bd50019cdef370e0b3f08a1e4ec0dcf1961
Jacob Lorenzen <jacob@lorenzen.me>
2021-05-15 07:11:26 +0200

    Solution for day02 part 2

--- src/day02.py
+++ src/day02.py
@@ -1,10 +1,23 @@
-from typing import List, Set, Dict, Tuple, Optional
+from typing import List
+
+
+def parse(s: str) -> List[int]:
+    return sorted(map(int, s.split('x')))
 
 
 def wrapping_paper(strs: List[str]) -> int:
     """find the amount of wrapping paper"""
     sum = 0
     for s in strs:
-        [l, w, h] = sorted(map(int, s.split('x')))
+        [l, w, h] = parse(s)
         sum += 2*l*w + 2*w*h + 2*h*l + l*w
+    return sum
+
+
+def calculate_bowtie(strs: List[str]) -> int:
+    """find the amount of bowtie"""
+    sum = 0
+    for s in strs:
+        [l, w, h] = parse(s)
+        sum += l*w*h+l+l+w+w
     return sum
--- tests/test_day02.py
+++ tests/test_day02.py
@@ -13,12 +13,23 @@ class Day02TestSuite(unittest.TestCase):
         ('2x3x4', 58),
         ('1x1x10', 43),
     ])
-    def test_wrapping_papaer(self, candidate, expected):
+    def test_wrapping_paper(self, candidate, expected):
         self.assertEquals(day02.wrapping_paper([candidate]), expected)
+
+    @parameterized.expand([
+        ('2x3x4', 34),
+        ('1x1x10', 14),
+    ])
+    def test_bowtie(self, candidate, expected):
+        self.assertEquals(day02.calculate_bowtie([candidate]), expected)
 
     def test_part1(self):
         f = open('data/day02.txt')
         self.assertEquals(day02.wrapping_paper(f.readlines()), 1588178)
+
+    def test_part2(self):
+        f = open('data/day02.txt')
+        self.assertEquals(day02.calculate_bowtie(f.readlines()), 3783758)
 
 
 if __name__ == '__main__':
