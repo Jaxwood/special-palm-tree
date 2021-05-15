122b5b3b983461403dde41a4fe1ab933f0538c22
Jacob Lorenzen <jacob@lorenzen.me>
2021-05-15 09:59:54 +0200

    Solution for day04 part 2

--- src/day04.py
+++ src/day04.py
@@ -1,12 +1,12 @@
 import hashlib
 
 
-def hash_of(s: str) -> str:
+def hash_of(s: str, pattern: str) -> str:
     i = 0
     while True:
         x = s + str(i)
         hash = hashlib.md5(x.encode()).hexdigest()
-        if hash[:5] == '00000':
+        if hash[:(len(pattern))] == pattern:
             return i
         else:
             i += 1
--- tests/test_day04.py
+++ tests/test_day04.py
@@ -14,10 +14,13 @@ class Day04TestSuite(unittest.TestCase):
         ('pqrstuv', 1048970),
     ])
     def test_find_md5_hash(self, candidate, expected):
-        self.assertEquals(day04.hash_of(candidate), expected)
+        self.assertEquals(day04.hash_of(candidate, '00000'), expected)
 
     def test_part1(self):
-        self.assertEquals(day04.hash_of('ckczppom'), 117946)
+        self.assertEquals(day04.hash_of('ckczppom', '00000'), 117946)
+
+    def test_part2(self):
+        self.assertEquals(day04.hash_of('ckczppom', '000000'), 3938038)
 
 
 if __name__ == '__main__':
