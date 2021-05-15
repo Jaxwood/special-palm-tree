bf1c96a0d2fb5d85a46891246dfa575e9c907eea
Jacob Lorenzen <jacob@lorenzen.me>
2021-05-15 09:23:39 +0200

    Add solution for day03 part 2

--- src/day03.py
+++ src/day03.py
@@ -15,3 +15,28 @@ def houses(path: str) -> int:
         current = (x, y)
         visited.add(current)
     return len(visited)
+
+
+def robot_houses(path: str) -> int:
+    """find all houses visited atleast once
+    with the robot as help"""
+    santa = (0, 0)
+    robot = (0, 0)
+    visited = {santa}
+    for i in range(0, len(path)):
+        x, y = santa if i % 2 == 0 else robot
+        s = path[i]
+        if s == '>':
+            x += 1
+        if s == '<':
+            x -= 1
+        if s == '^':
+            y += 1
+        if s == 'v':
+            y -= 1
+        if i % 2 == 0:
+            santa = (x, y)
+        else:
+            robot = (x, y)
+        visited.add((x, y))
+    return len(visited)
--- tests/test_day03.py
+++ tests/test_day03.py
@@ -17,9 +17,21 @@ class Day03TestSuite(unittest.TestCase):
     def test_find_houses_visited_once(self, candidate, expected):
         self.assertEquals(day03.houses(candidate), expected)
 
+    @parameterized.expand([
+        ('^v', 3),
+        ('^>v<', 3),
+        ('^v^v^v^v^v', 11),
+    ])
+    def test_find_houses_visited_with_robot_once(self, candidate, expected):
+        self.assertEquals(day03.robot_houses(candidate), expected)
+
     def test_part1(self):
         f = open('data/day03.txt')
         self.assertEquals(day03.houses(f.read()), 2081)
+
+    def test_part2(self):
+        f = open('data/day03.txt')
+        self.assertEquals(day03.robot_houses(f.read()), 2341)
 
 
 if __name__ == '__main__':
