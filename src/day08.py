1f759f4ca09d4d8fc2b404b4e231fe8b45e52124
Jacob Lorenzen <jacob@lorenzen.me>
2021-05-17 08:13:07 +0200

    Solution for day08 part 2

--- src/day08.py
+++ src/day08.py
@@ -34,3 +34,38 @@ def storage_amount(lines: List[str]) -> int:
                 memory += 1
                 i += 1
     return size - memory
+
+
+def storage_escaped_amount(lines: List[str]) -> int:
+    # total length except begin and end
+    escaped = 0
+    size = 0
+    for line in lines:
+        i = 0
+        while i < len(line):
+           # check begin and end of line
+            if i == 0 or i == len(line) - 1:
+                escaped += 3
+                size += 1
+                i += 1
+            # check for hexadecimal, e.g. \x27
+            elif line[i] == '\\' and line[i+1] == 'x':
+                escaped += 5
+                size += 4
+                i += 4
+            # check for escaped quote
+            elif line[i] == '\\' and line[i+1] == '"':
+                escaped += 4
+                size += 2
+                i += 2
+            # check for escaped backslash
+            elif line[i] == '\\' and line[i+1] == '\\':
+                escaped += 4
+                size += 2
+                i += 2
+            # otherwise increase by one
+            else:
+                escaped += 1
+                size += 1
+                i += 1
+    return escaped - size
--- tests/test_day08.py
+++ tests/test_day08.py
@@ -16,10 +16,23 @@ class Day08TestSuite(unittest.TestCase):
         self.assertEquals(day08.storage_amount(
             string_literal), 12)
 
+    def test_storage_escaped_amount(self):
+        string_literal = [r'""',
+                          r'"abc"',
+                          r'"aaa\"aaa"',
+                          r'"\x27"']
+        self.assertEquals(day08.storage_escaped_amount(
+            string_literal), 19)
+
     def test_part1(self):
         f = open('data/day08.txt')
         self.assertEquals(day08.storage_amount(
             f.read().splitlines()), 1350)
+
+    def test_part2(self):
+        f = open('data/day08.txt')
+        self.assertEquals(day08.storage_escaped_amount(
+            f.read().splitlines()), 2085)
 
 
 if __name__ == '__main__':
