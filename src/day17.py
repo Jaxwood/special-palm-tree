2935d7ee19b954e867530d3f173881112ece9af9
Jacob Lorenzen <jacob@lorenzen.me>
2021-05-21 20:16:15 +0200

    Solution for day17 part 2

--- src/day17.py
+++ src/day17.py
@@ -1,15 +1,20 @@
-from typing import List
+from typing import List, Tuple
 
 
-def combi(nums: List[int], total: int, acc: int) -> int:
-    """recursively call the combi function to calculate"""
+def combi(nums: List[int], total: int, acc: List[int]) -> List[int]:
     if total < 0:
-        return 0
+        return []
     if total == 0:
-        return 1
+        return [acc]
     if len(nums) == 0:
-        return acc
+        return []
 
     n, rest = nums[0], nums[1:]
 
-    return combi(rest, total - n, acc) + combi(rest, total, acc)
+    return combi(rest, total - n, acc+1) + combi(rest, total, acc)
+
+
+def combi_min_length(nums: List[int], total: int, acc: int) -> int:
+    result = combi(nums, total, acc)
+    lowest = min(result)
+    return len(list(filter(lambda x: x == lowest, result)))
--- tests/test_day17.py
+++ tests/test_day17.py
@@ -10,11 +10,21 @@ class Day17TestSuite(unittest.TestCase):
     """Test Suite for Day17"""
 
     def test_combi(self):
-        self.assertEquals(day17.combi([20, 15, 10, 5, 5], 25, 0), 4)
+        self.assertEquals(
+            len(day17.combi([20, 15, 10, 5, 5], 25, 0)), 4)
+
+    def test_combi_min_length(self):
+        actual = day17.combi_min_length([20, 15, 10, 5, 5], 25, 0)
+        self.assertEquals(actual, 3)
 
     def test_part1(self):
-        self.assertEquals(day17.combi([43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34,
-                                       17, 17, 44, 36, 31, 46, 9, 27, 38], 150, 0), 1638)
+        self.assertEquals(len(day17.combi([43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34,
+                                           17, 17, 44, 36, 31, 46, 9, 27, 38], 150, 0)), 1638)
+
+    def test_part2(self):
+        actual = day17.combi_min_length([43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34,
+                                         17, 17, 44, 36, 31, 46, 9, 27, 38], 150, 0)
+        self.assertEquals(actual, 17)
 
 
 if __name__ == '__main__':
