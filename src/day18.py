aa1e2b8b1967028223a0ae76c2389a69290d3f72
Jacob Lorenzen <jacob@lorenzen.me>
2021-05-21 22:11:57 +0200

    Add solution for day18 part 2

--- src/day18.py
+++ src/day18.py
@@ -1,9 +1,11 @@
 from typing import Dict, List, Tuple
+import math
 
 Grid = Dict[Tuple[int, int], str]
 
 
 def parse(raw: List[str]) -> Grid:
+    """parse initial grid"""
     result = {}
     for i, line in enumerate(raw):
         for j, c in enumerate(line):
@@ -12,31 +14,51 @@ def parse(raw: List[str]) -> Grid:
 
 
 def tuple_sum(a: Tuple[int, int], b: Tuple[int, int]) -> Tuple[int, int]:
+    """add two tuples"""
     [x, y] = a
     [z, w] = b
     return (x+z, y+w)
 
 
-def tick(grid: Grid) -> Grid:
+def is_edge(grid: Grid, coord: Tuple[int, int]) -> bool:
+    """check if an edge coordinate"""
+    wide = int(math.sqrt(len(grid))) - 1
+    return coord in set([(0, 0), (0, wide), (wide, 0), (wide, wide)])
+
+
+def tick(grid: Grid, edge: bool = False) -> Grid:
+    """simulate one step"""
     result: Grid = {}
     neighbors = [(1, 0), (0, 1), (1, 1), (-1, -1),
                  (-1, 0), (0, -1), (-1, 1), (1, -1)]
     for g in grid:
         next = map(lambda x: tuple_sum(x, g), neighbors)
         on = 0
+        # count how many lights are on
         for n in next:
             candidate = grid.get(n)
             if candidate != None and candidate == '#':
                 on = on + 1
-        if grid.get(g) == '#':
+
+        # update state
+        if edge and is_edge(grid, g):
+            result[g] = '#'
+        elif grid.get(g) == '#':
             result[g] = '#' if on == 2 or on == 3 else '.'
         else:
             result[g] = '#' if on == 3 else '.'
     return result
 
 
-def light_switcher(raw: List[str], steps: int) -> int:
+def light_switcher(raw: List[str], steps: int, edge: bool = False) -> int:
+    """runs simulation for x steps"""
     grid = parse(raw)
+    # set edges on
+    if edge:
+        for g in grid:
+            if is_edge(grid, g):
+                grid[g] = '#'
+    # run simulation
     for _ in range(steps):
-        grid = tick(grid)
+        grid = tick(grid, edge)
     return len(list(filter(lambda x: x == '#', grid.values())))
--- tests/test_day18.py
+++ tests/test_day18.py
@@ -20,10 +20,26 @@ class Day18TestSuite(unittest.TestCase):
         ]
         self.assertEquals(day18.light_switcher(grid, 4), 4)
 
+    def test_light_switcher_with_edge(self):
+        grid = [
+            '.#.#.#',
+            '...##.',
+            '#....#',
+            '..#...',
+            '#.#..#',
+            '####..',
+        ]
+        self.assertEquals(day18.light_switcher(grid, 5, True), 17)
+
     def test_part1(self):
         f = open('data/day18.txt')
         self.assertEquals(day18.light_switcher(
             f.read().splitlines(), 100), 821)
+
+    def test_part2(self):
+        f = open('data/day18.txt')
+        self.assertEquals(day18.light_switcher(
+            f.read().splitlines(), 100, True), 886)
 
 
 if __name__ == '__main__':
